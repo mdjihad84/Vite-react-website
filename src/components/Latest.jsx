ba2dde1b6a2760804c63b76e7fe80b551e52b25e
MD JAHIDUL ISLAM JIHAD <mdj847621@gmail.com>
2024-10-02 06:50:03 +0600

    update latest area cards

--- src/components/Latest.jsx
+++ src/components/Latest.jsx
@@ -1,16 +1,21 @@
+import Latest1 from '../images/Latest1.png';
+import Latest2 from '../images/Latest2.png';
+import Latest3 from '../images/Latest3.png';
+
 function Latest() {
   return (
     <div className="bg-white py-10">
       <div className="container mx-auto px-4">
         <div className="text-center mb-10">
-          <h1 className="text-5xl font-semibold text-black my-8">latest News</h1>
+          <h1 className="text-5xl font-semibold text-black my-8">Latest News</h1>
         </div>
 
-        {/* card number (1) */}
+        {/* Cards */}
         <div className="flex flex-wrap justify-center gap-6 mt-6">
+          {/* Card 1 */}
           <div className="card shadow-xl p-6 border border-solid w-[25rem]">
             <figure>
-              <img src="../images/Latest1.png" alt="Ducati XDIAVEL S-73" className="w-full"/>
+              <img src={Latest1} alt="Ducati XDIAVEL S-73" className="w-full"/>
             </figure>
             <div className="card-body">
               <h2 className="card-title text-black font-bold">Ducati XDIAVEL S-73</h2>
@@ -21,10 +26,10 @@ function Latest() {
             </div>
           </div>
 
-          {/* card number (2)*/}
+          {/* Card 2 */}
           <div className="card shadow-xl p-6 border border-solid w-[25rem]">
             <figure>
-              <img src="../images/Latest2.png" alt="Motorcycles & Scooters" className="w-full"/>
+              <img src={Latest2} alt="Motorcycles & Scooters" className="w-full"/>
             </figure>
             <div className="card-body">
               <h2 className="card-title text-black font-bold">Motorcycles & Scooters</h2>
@@ -35,10 +40,10 @@ function Latest() {
             </div>
           </div>
 
-          {/* card number (3) */}
+          {/* Card 3 */}
           <div className="card shadow-xl p-6 border border-solid w-[25rem]">
             <figure>
-              <img src="../images/Latest3.png" alt="2021 Honda CBR500R" className="w-full"/>
+              <img src={Latest3} alt="2021 Honda CBR500R" className="w-full"/>
             </figure>
             <div className="card-body">
               <h2 className="card-title text-black font-bold">2021 Honda CBR500R</h2>
@@ -53,4 +58,5 @@ function Latest() {
     </div>
   );
 }
+
 export default Latest;
